777a51fd78fe01b9e574ccc658e7fce51dcf13dd
Robert <Roberttovarv@gmail.com>
2025-10-22 13:59:02 +0200

    Added rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    float rotation = .6f;
+    InputAction moveAction;
+    Rigidbody2D myRigidBody;
 
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 moveVector;
+        moveVector = moveAction.ReadValue<Vector2>();
+        if (moveVector.x < 0) myRigidBody.AddTorque(rotation);
+        if (moveVector.x > 0) myRigidBody.AddTorque(-rotation);
+
+    }
 }
